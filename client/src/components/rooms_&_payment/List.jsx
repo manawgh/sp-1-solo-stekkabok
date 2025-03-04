071e31272490fff8414c5d108874f5b434929b7c
Paul Paumier Martinez <manaweb@posteo.net>
2025-03-04 14:51:45 +0100

    room-list css fixed

--- client/src/components/rooms_&_payment/List.jsx
+++ client/src/components/rooms_&_payment/List.jsx
@@ -6,7 +6,7 @@ function List ({availableRooms, days, triggerWarning}) {
   return (
     <div className='room-list'>
       {availableRooms.map( room => <Room key={room.name} name={room.name} prod_id={room.prod_id} image={room.imgPath} price={room.price} desc={room.desc} sleeps={room.sleeps} days={days} triggerWarning={triggerWarning}/>)}
-  </div>
+    </div>
   )
 }
 
